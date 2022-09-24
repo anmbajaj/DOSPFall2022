45d1aa2cfb93f88bb1c9b302d172651555418da8
Anmol Bajaj <anmbajaj@gmail.com>
2022-09-24 04:11:54 -0400

    Added termination functionality, replaced the hardcoded IP values and rectified code structure

--- bitcoin-miner/Master/util.erl
+++ bitcoin-miner/Master/util.erl
@@ -17,7 +17,7 @@
 -define(RANDOM_STRING_LENGTH, 15).
 -define(ALLOWED_CHARS, "qwertyQWERTY1234567890").
 
-%Add code for random string generation
+%Random string generation
 get_random_string(Length, AllowedChars) ->
   lists:foldl(fun(_, Acc) ->
     [lists:nth(rand:uniform(length(AllowedChars)),
@@ -25,35 +25,36 @@ get_random_string(Length, AllowedChars) ->
     ++ Acc
               end, [], lists:seq(1, Length)).
 
-%Add code for string matching
+%String pattern matching
 pattern_match_string(_, TotalZeros, TotalZeros) -> true;
 pattern_match_string([48 | T], Current, TotalZeros) ->
   pattern_match_string(T, Current+1, TotalZeros);
 pattern_match_string(_, _, _) -> false.
 
-
 %SHA256 computation code
 calculateSHA256(String) ->
     io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256, [String]))]).
 
-
-%runner
-runner(_, _, Workload, Workload, _) -> true;
-runner(Sender, TotalZeros, Count, Workload, ActorPID) ->
-  String = get_random_string(?RANDOM_STRING_LENGTH, ?ALLOWED_CHARS),
+%Runner
+runner(Sender, _, Workload, Workload) ->
+  Sender ! {self(), actor_work_completed},
+  exit(self());
+runner(Sender, TotalZeros, Count, Workload) ->
+  RandomString = get_random_string(?RANDOM_STRING_LENGTH, ?ALLOWED_CHARS),
+  String = concat(?GATOR_ID, RandomString),
   HashString = calculateSHA256(String),
   case pattern_match_string(HashString, 0, TotalZeros) of
     true ->
-      ActorPID   ! {concat(concat(?GATOR_ID, String), concat("        ", HashString))};
+      Sender ! {self(), bitcoin_found, String, HashString};
     false ->
       ok
   end,
-  runner(Sender, TotalZeros, (Count+1), Workload, ActorPID).
+  runner(Sender, TotalZeros, (Count+1), Workload).
 
+%Start
 start() ->
   receive
-    {Sender, start, TotalZeros, Workload, ActorPID} ->
-      runner(Sender, TotalZeros, 0, Workload, ActorPID)
+    {Sender, start, TotalZeros, Workload} ->
+      runner(Sender, TotalZeros, 0, Workload)
   end,
   start().
-
